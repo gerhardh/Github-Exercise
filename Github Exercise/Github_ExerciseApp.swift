196c5bae9dad341b8741ac986450369bc989aacb
Gerhard Heidkamp <gheidkamp@mac.com>
2023-08-15 11:34:56 +0200

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Gerhard Heidkamp (dev) on 15.08.23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
